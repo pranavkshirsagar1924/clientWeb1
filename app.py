eb7e0ba65db0387d9ba8cf37144ceb5eac4d8be1
Pranav Kshirsagar <128245885+pranavkshirsagar1924@users.noreply.github.com>
2024-11-16 18:07:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USERNAME'] = f'{m}'
 app.config['MAIL_PASSWORD'] = f'{p}'
-app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 mail = Mail(app)
 
