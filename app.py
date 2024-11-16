1301b2b01bc23aef980528ec182f1a1e486ad40d
Pranav Kshirsagar <128245885+pranavkshirsagar1924@users.noreply.github.com>
2024-11-16 18:05:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app.config['MAIL_PORT'] = 465
 app.config['MAIL_USERNAME'] = f'{m}'
 app.config['MAIL_PASSWORD'] = f'{p}'
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USE_SSL'] = False
+app.config['MAIL_USE_SSL'] = True
 mail = Mail(app)
 
 @app.route('/')
